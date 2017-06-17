ee6fecb5795641701f2bb744eda7d96ed2a00185
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-06-17 05:42:51 -0500

    Added Classification and control to toggle between rectangles and classifications.

--- VisionFun/VideoCaptureVC.swift
+++ VisionFun/VideoCaptureVC.swift
@@ -8,6 +8,10 @@ enum VideoCaptureError {
 
 class VideoCaptureViewController: UIViewController {
     
+    // MARK: - Outlets
+    
+    @IBOutlet var classificationLabel: UILabel!
+    
     // MARK: - Stored Properties
     
     private var captureSession: AVCaptureSession?
@@ -15,6 +19,7 @@ class VideoCaptureViewController: UIViewController {
     fileprivate var previewLayer: AVCaptureVideoPreviewLayer?
     
     private var requests = [VNRequest]()
+    
     private let shapeLayer = CAShapeLayer()
     
     // MARK: - Computed Properties
@@ -77,6 +82,7 @@ class VideoCaptureViewController: UIViewController {
             try addPreviewLayer(for: captureSession!, to: self.view)
             try addVideoDataOutput(to: captureSession!)
             addShapeLayer()
+            classificationLabel.isHidden = true
         } catch {
             print(error)
         }
@@ -134,14 +140,44 @@ class VideoCaptureViewController: UIViewController {
     
     // MARK: - Vision
     
+    private var classificationRequest: VNCoreMLRequest?
+    private var rectangleDetectionRequest: VNDetectRectanglesRequest?
+    
     private func setupVision() {
-        let rectangleDetectionRequest = VNDetectRectanglesRequest(
+        
+        // Classifications
+        
+        guard let visionModel = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Failed to load ML model")
+        }
+        self.classificationRequest = VNCoreMLRequest(
+            model: visionModel,
+            completionHandler: handleClassifications
+        )
+        classificationRequest?.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop
+        
+        // Rectangles
+        
+        self.rectangleDetectionRequest = VNDetectRectanglesRequest(
             completionHandler: self.handleRectangles
         )
-        rectangleDetectionRequest.minimumSize = 0.2
-        rectangleDetectionRequest.maximumObservations = 10
+        rectangleDetectionRequest?.minimumSize = 0.2
+        rectangleDetectionRequest?.maximumObservations = 5
         
-        self.requests = [rectangleDetectionRequest]
+        self.requests = [
+            rectangleDetectionRequest!
+        ]
+    }
+    
+    func handleClassifications(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNClassificationObservation] else { return }
+        
+        let classifications = observations[0...4].filter({ $0.confidence > 0.3 })
+        let classificationMessages = classifications.map({ "\($0.identifier)\nconfidence: \($0.confidence)" })
+        
+        DispatchQueue.main.async {
+            self.classificationLabel.text = classificationMessages.joined(separator: "\n\n")
+        }
     }
     
     func handleRectangles(request: VNRequest, error: Error?) {
@@ -165,6 +201,28 @@ class VideoCaptureViewController: UIViewController {
         path.close()
         
         shapeLayer.path = path.cgPath
+    }
+    
+    
+    @IBAction func selectionChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: // Rectangles
+            guard let rectangleDetectionRequest = self.rectangleDetectionRequest else { return }
+            requests = [rectangleDetectionRequest]
+            
+            classificationLabel.isHidden = true
+            shapeLayer.isHidden = false
+            
+        case 1: // Classifications
+            guard let classificationRequest = self.classificationRequest else { return }
+            requests = [classificationRequest]
+            
+            classificationLabel.isHidden = false
+            shapeLayer.isHidden = true
+            
+        default:
+            break
+        }
     }
 }
 
